011016db37bf26cd9b7268b5ea387ad8603978a3
Brandon Burton <brandon@inatree.org>
2015-03-10 20:16:46 -0700

    testing

--- scripts/build.rb
+++ scripts/build.rb
@@ -6,6 +6,7 @@ require 'toml'
 
 def make_package(package_name, package_info)
   source_url = package_info["source"]
+  version = package_info["version"]
   source_file_name = source_url.split("/")[-1]
 
   get_source_cmd = Mixlib::ShellOut.new("curl -s #{source_url} -o /tmp/#{source_file_name} ")
@@ -17,6 +18,14 @@ def make_package(package_name, package_info)
   elsif File.exist?("/tmp/#{source_file_name}") == true
     puts "Download of #{source_url} successful"
   end
+
+  if source_file_name.include? "tar"
+    tar_dst = "/tmp/#{package_name}_#{version}"
+    Dir.mkdir(tar_dst) unless File.exists?(tar_dst)
+    untar_cmd = Mixlib::ShellOut.new("tar -xf /tmp/#{source_file_name} --strip-components=1 -C #{tar_dst}")
+    untar_cmd.run_command
+  end
+
 end
 
 packages_file = "packages/packages.toml"
