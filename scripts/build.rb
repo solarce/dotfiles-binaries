7f11fd61def489d8b04394923df3665ecb45f539
Brandon Burton <brandon@inatree.org>
2015-03-12 14:00:55 -0700

    testing

--- scripts/build.rb
+++ scripts/build.rb
@@ -5,12 +5,17 @@ require "mixlib/shellout"
 require 'toml'
 
 def make_package(package_name, package_info)
-  pkgsrc_root = "#{ENV['HOME']}/pkg" 
+  pkgsrc_root = "#{ENV['HOME']}/pkg"
   #source_url = package_info["source"]
-  version = package_info["version"]
+  pkgsrc_name = package_info["pkgsrc_name"]
+  #version = package_info["version"]
   #source_file_name = source_url.split("/")[-1]
 
   p Dir["#{pkgsrc_root}/*"]
+  make_pkg_cmd = Mixlib::ShellOut.new("bmake", "package clean clean-depends", :env => "USE_DESTDIR=yes", :cwd => "#{pkgsrc_root}/#{pkgsrc_name}")
+  #env USE_DESTDIR=yes bmake package clean clean-depends
+  make_pkg_cmd.run_command # etc.
+  p make_pkg_cmd.stdout
 end
 
 packages_file = "packages/packages.toml"
