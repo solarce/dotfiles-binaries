368bd09c4d2e4d9f51c4246a80417f96d82931ce
Brandon Burton <brandon@inatree.org>
2015-03-12 14:07:44 -0700

    testing

--- scripts/build.rb
+++ scripts/build.rb
@@ -11,10 +11,12 @@ def make_package(package_name, package_info)
   #version = package_info["version"]
   #source_file_name = source_url.split("/")[-1]
 
-  p Dir["#{pkgsrc_root}/*"]
+  #p Dir["#{pkgsrc_root}/*"]
+  puts "building package for #{pkgsrc_name}"
   make_pkg_cmd = Mixlib::ShellOut.new("bmake", "package clean clean-depends", :env => "USE_DESTDIR=yes", :cwd => "#{pkgsrc_root}/#{pkgsrc_name}")
   #env USE_DESTDIR=yes bmake package clean clean-depends
   make_pkg_cmd.run_command # etc.
+  p make_pkg_cmd
   p make_pkg_cmd.stdout
 end
 
