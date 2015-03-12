b96ff2d005646cbc85964848de6768608c2aa598
Brandon Burton <brandon@inatree.org>
2015-03-12 11:10:55 -0700

    testing

--- scripts/build.rb
+++ scripts/build.rb
@@ -5,28 +5,12 @@ require "mixlib/shellout"
 require 'toml'
 
 def make_package(package_name, package_info)
-  source_url = package_info["source"]
+  pkgsrc_root = "#{ENV['HOME']}/pkg" 
+  #source_url = package_info["source"]
   version = package_info["version"]
   source_file_name = source_url.split("/")[-1]
 
-  get_source_cmd = Mixlib::ShellOut.new("curl -s #{source_url} -o /tmp/#{source_file_name} ")
-  puts "Downloading #{source_url} to /tmp/#{source_file_name}"
-  get_source_cmd.run_command
-  if get_source_cmd.exitstatus == 1
-    puts "Download of #{source_url} failed"
-    exit(1)
-  elsif File.exist?("/tmp/#{source_file_name}") == true
-    puts "Download of #{source_url} successful"
-  end
-
-  if source_file_name.include? "tar"
-    tar_dst = "/tmp/#{package_name}_#{version}"
-    Dir.mkdir(tar_dst) unless File.exists?(tar_dst)
-    untar_cmd = Mixlib::ShellOut.new("tar -xf /tmp/#{source_file_name} --strip-components=1 -C #{tar_dst}")
-    untar_cmd.run_command
-  end
-
-
+  p Dir["#{pkgsrc_root}/*"]
 end
 
 packages_file = "packages/packages.toml"
