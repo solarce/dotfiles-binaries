59204211b546392e1c51a06c2a602196a7b0ccf9
Brandon Burton <brandon@inatree.org>
2015-03-14 23:43:55 -0400

    testing with caching

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -9,6 +9,7 @@ mkdir build
 # the contents of pkgsrc to a Volume mount that CI will cache for us
 if [ ! -f /home/ubuntu/cache/pkgsrc/synced.txt ]
 then
+  echo "Syncing pkgsrc"
   rsync -aP -q /home/ubuntu/pkgsrc/ /home/ubuntu/cache/pkgsrc/
   rm -r /home/ubuntu/pkgsrc
   ln -s /home/ubuntu/cache/pkgsrc /home/ubuntu/pkgsrc
@@ -16,6 +17,7 @@ fi
 
 if [ ! -d /home/ubuntu/cache/packages ]
 then
+  echo "Making packages cache"
   mkdir -p cache/packages
   rm -r /home/ubuntu/pkgsrc/packages
   ln -s /home/ubuntu/cache/packages /home/ubuntu/pkgsrc/packages
